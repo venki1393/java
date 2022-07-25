887e0bc31ff6e82611f81621793fe0de89e10fd5
venkatesh <venki1393@gmail.com>
2022-07-25 15:31:16 +0530

    First Commit

---
+++ src/main/java/com/firefly/department/service/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.firefly.department.service.controller;
+
+import com.firefly.department.service.entity.Department;
+import com.firefly.department.service.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department){
+        log.info("Inside saveDepartment method of DepartmentController");
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartmentById(@PathVariable("id") Long departmentId){
+        log.info("Inside findDepartmentById method of DepartmentController");
+        return departmentService.findDepartmentById(departmentId);
+    }
+}

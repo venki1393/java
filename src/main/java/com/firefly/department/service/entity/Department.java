be7b7f1b166eee1066438f232f359b709173d200
venkatesh <venki1393@gmail.com>
2022-07-25 15:48:19 +0530

    First Commit

--- src/main/java/com/firefly/department/service/entity/Department.java
+++ src/main/java/com/firefly/department/service/entity/Department.java
@@ -19,5 +19,5 @@ public class Department {
     private Long departmentId;
     private String departmentName;
     private String departmentAddress;
-    private String departmentCode;
+    private String departmentCode; 
 }
